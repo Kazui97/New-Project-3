eb048b9202ace910659289218dbfd51c9dc82429
Kazui97 <camyescobar12@gmail.com>
2019-11-29 23:26:46 -0500

    torre 1 y plataformas ya

---
+++ Assets/scrist/Construirmanager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Construirmanager : MonoBehaviour
+{
+    public static Construirmanager instance;
+    private GameObject cañonconstruido;
+    public GameObject cañonprefab;
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("hay un erro :;v hay mas en la escena");
+            return;
+        }
+        instance = this;
+    }
+    void Start()
+    {
+        cañonconstruido = cañonprefab;
+    }
+    public GameObject cañonparaconstruir()
+    {
+        return cañonconstruido;
+    }
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scrist/Plataformas.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataformas : MonoBehaviour
+{
+    public Color prescolor;
+    private Renderer rend;
+    private Color starcolor;
+    private GameObject caññon;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        starcolor = rend.material.color;
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if(caññon != null)
+        {
+            Debug.LogError("no puede contruir");
+            return;
+        }
+        GameObject cannontobuild = Construirmanager.instance.cañonparaconstruir();
+        caññon = (GameObject)Instantiate(cannontobuild, transform.position, transform.rotation);
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = prescolor;
+    }
+    private void OnMouseExit()
+    {
+        rend.material.color = starcolor;
+    }
+
+    void Update()
+    {
+        
+    }
+}
